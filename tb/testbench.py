bf4093d1ac554326f26a7fdcbb7aed31c67cf088
IAmMarcelJung <MarcelJung97@web.de>
2025-08-22 00:29:57 +0200

    test: reuse the test functions defined testbenches of the wrapped projects

--- tb/testbench.py
+++ tb/testbench.py
@@ -9,59 +9,23 @@ import importlib.util
 import cocotb
 from cocotb.clock import Clock
 from cocotb.runner import get_runner
-from cocotb.triggers import Timer, ClockCycles, RisingEdge
+from cocotb.triggers import Timer, RisingEdge
 
 # ------------------------------
-# Import the PPWM testbench from the submodule
-#
-# Needs special handling due to the dash in the path
+# Import the testbenches of the wrapped projects
 # ------------------------------
+
 this_path = Path(__file__).resolve().parent
-ppwm_tb_path = this_path.parent / "submodules/heichips25-ppwm/tb/testbench.py"
 
+ppwm_tb_path = this_path.parent / "submodules/heichips25-ppwm/tb/testbench.py"
 spec = importlib.util.spec_from_file_location("ppwm_tb", ppwm_tb_path)
 ppwm_tb = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(ppwm_tb)
 
-# Initialize memory with program instructions
-# 16 x 7-bit instructions
-PROGRAM = [
-    0b011_0_001,  # 0 set pwm 3
-    0b011_1_001,  # 1 set reg 3
-    0b0_001_110,  # 2 cmp gcntl < reg
-    0b_0010_111,  # 3 branch #5
-    0b_0010_101,  # 4 jump #6
-    0b001_0_010,  # 5 add pwm 1
-    0b_0001_000,  # 6 wait
-    0b_1010_101,  # 7 jump #2
-    0b0000000,  # ctrl nop
-    0b0000000,  # ctrl nop
-    0b0000000,  # ctrl nop
-    0b0000000,  # ctrl nop
-    0b0000000,  # ctrl nop
-    0b0000000,  # ctrl nop
-    0b0000000,  # ctrl nop
-    0b0000000,  # ctrl nop
-]
-
-PROGRAM_TEST_MV = [
-    0b011_1_001,  # 0 set reg, 3
-    0b11_1_1_011,  # 1 shift reg left
-    0b00_1_1_011,  # 2 shift reg left
-    0b00_1_1_011,  # 3 shift reg left
-    0b0_001_110,  # 4 cmp global counter < reg
-    0b0011_111,  # 5 branch #8
-    0b0_000_100,  # 6 mv reg to pwm
-    0b0010_101,  # 7 jump +2
-    0b0_100_100,  # 8 mv global counter l to pwm
-    0b0001_000,  # 9 ctrl wait
-    0b1010_101,  # a jump #4
-    0b0000000,  # b
-    0b0000000,  # c
-    0b0000000,  # d
-    0b0000000,  # e
-    0b0000000,  # f
-]
+sdr_tb_path = this_path.parent / "submodules/heichips25_SDR_new/tb/testbench.py"
+spec = importlib.util.spec_from_file_location("sdr_tb", sdr_tb_path)
+sdr_tb = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(sdr_tb)
 
 
 @cocotb.test()
@@ -84,63 +48,11 @@ async def compare_wrapper_vs_gold(dut):
 
     cocotb.start_soon(checker(dut))
 
-    await exec_ppwm_test(dut)
+    await ppwm_tb.pwm_test(dut)
 
     dut.ena.value = 1  # Disable PPWM, enable SDR
 
-    await exec_sdr_test(dut)
-
-
-async def exec_ppwm_test(dut):
-    """Execute the test for the ppwm module."""
-    # Load program into memory
-    await ClockCycles(dut.clk, 1)
-    await ppwm_tb.load_program_to_memory(dut, PROGRAM)
-    await ClockCycles(dut.clk, 2)
-    # Wait for another half period to complete the cycle
-    await ClockCycles(dut.clk, 2048 * 24)
-
-    # Reset the design for 10ns
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 1)
-    dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 1)
-    # Load program into memory
-    await ppwm_tb.load_program_to_memory(dut, PROGRAM_TEST_MV)
-    await ClockCycles(dut.clk, 2)
-    # Wait for another half period to complete the cycle
-    await ClockCycles(dut.clk, 2048 * 24)
-
-
-async def exec_sdr_test(dut):
-    """Execute the test for the sdr module."""
-    await apply_sdr_inputs(0, 2, 0, 2, dut)
-    await ClockCycles(dut.clk, 1)
-    await apply_sdr_inputs(0, 2, 0, 2, dut)
-    await ClockCycles(dut.clk, 1)
-    await apply_sdr_inputs(0, 2, 0, 2, dut)
-    await ClockCycles(dut.clk, 1)
-    await apply_sdr_inputs(0, 2, 0, 2, dut)
-    await ClockCycles(dut.clk, 1)
-    await apply_sdr_inputs(0, 2, 0, 2, dut)
-    await ClockCycles(dut.clk, 1)
-    await apply_sdr_inputs(0, 0, 0, 0, dut)
-
-    # Wait for 10 clock cycles
-    await ClockCycles(dut.clk, 10)
-
-    # Enable the counter
-    dut.ui_in.value = 1
-
-    # Wait for 10 clock cycles
-    await ClockCycles(dut.clk, 10)
-
-
-async def apply_sdr_inputs(I1, Q1, I2, Q2, dut):
-    """Apply the test stimuli to the sdr module."""
-    dut.heichips25_tiny_wrapper_i.ui_in.value = Q1 << 4 | I1
-    dut.heichips25_tiny_wrapper_i.uio_in.value = Q2 << 4 | I2
-    await ClockCycles(dut.clk, 1)
+    await sdr_tb.sdr_test(dut)
 
 
 async def check_wrapper_vs_dut_values(wrapper_value, project_value, project_name):
